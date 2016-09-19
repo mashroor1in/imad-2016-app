97fe2a8feeb0f329a363f74d7f523628159f051b
mashroor1in <mashroor1in@gmail.com>
2016-09-19 19:22:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-aap.get('/article-one', function(req,res){
+app.get('/article-one', function(req,res){
     res.send('Hello, this is article one');
 });
 
-aap.get('/article-two', function(req,res){
+app.get('/article-two', function(req,res){
     res.send('Hello, this is article two');
 });
 
-aap.get('/article-three', function(req,res){
+app.get('/article-three', function(req,res){
     res.send('Hello, this is article three');
 });
 
