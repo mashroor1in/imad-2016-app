0cc245ab6c6ba15161af9e3b7db639a647647d21
mashroor1in <mashroor1in@gmail.com>
2016-09-26 20:39:08 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,9 +3,15 @@ var element =document.getElementById('newText');
 element.innerHTML="New Value";
 
 var img=document.getElementById('image');
-img.onclick=function(){
+var marginleft=0;
+function moveLeft(){
+  marginleft=marginleft+10;
+  img.style.marginLeft=marginleft+'px';
   
-    img.style.marginRight='200px';
+}
+img.onclick=function(){
+    var interval=setInterval(moveLeft,200);
+   
     
     
 };
