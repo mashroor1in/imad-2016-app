59a5877c07313853c1ad396d3c23b42e78a72757
mashroor1in <mashroor1in@gmail.com>
2016-09-26 19:48:18 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Mash Loaded!');
 var element =document.getElementById('newText');
 element.innerHTML="New Value";
 
-var img=document.getElementById('image');
+var img=document.getElementById('img');
 img.onclick=function(){
     img.style.marginright='100px';
     
