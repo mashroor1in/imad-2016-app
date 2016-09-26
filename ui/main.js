b8a023628528d07f10370565333cf78a0f9542c6
mashroor1in <mashroor1in@gmail.com>
2016-09-26 20:40:26 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ element.innerHTML="New Value";
 var img=document.getElementById('image');
 var marginleft=0;
 function moveLeft(){
-  marginleft=marginleft+10;
+  marginleft=marginleft+1;
   img.style.marginLeft=marginleft+'px';
   
 }
