e9967c410257eb053f679c25cc02be5c3a1551fc
mashroor1in <mashroor1in@gmail.com>
2016-09-27 12:50:51 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,10 @@
-var button=document.getElementById('button');
+var button=document.getElementById('counter');
 var counter =0;
 
 
 button.onclick = function(){
     counter=counter+1;
-    var display=document.getElementById('snap1');
+    var display=document.getElementById('count');
     display.innerHTML=counter.toString();
     
 };
