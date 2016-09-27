1ebc4b5ac2a69e1bb9e98efdae82ccedbd64c9b2
mashroor1in <mashroor1in@gmail.com>
2016-09-27 19:45:44 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,20 +4,24 @@ var button=document.getElementById('counter');
 
 button.onclick = function(){
    
-   var request=new XMLHTTPRequest();
-   request.onreadystatechange= unction()
+   var request = new XMLHTTPRequest();
+   request.onreadystatechange= function()
    {
-        if(request.readyState===XMLHTTPResqest.Done)
+        if(request.readyState === XMLHttpRequest.Done)
         {
             if(request.satus===200)
             {
-                
+                var counter= request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML=counter.toString();
             }
         }
         
    
    
-   }
+   };
+   request.open('GET', 'mashroor1in.imad.hasura-app.io/counter', true);
+   request.send(null);
 };
 
 
