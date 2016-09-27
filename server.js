9ec96cd707783f6c63ab3a57d3356db201547784
mashroor1in <mashroor1in@gmail.com>
2016-09-27 12:29:05 +0000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,11 +92,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var counter=0;
-app.get('/counter', function(req,res){
-   counter=counter+1;
-   res.send(counter.toString());
-});
+
 
 app.get('/:articleName', function(req,res){
     var articleName=req.params.articleName;
@@ -115,6 +111,12 @@ app.get('/ui/style.css', function (req, res) {
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+var counter=0;
+app.get('/counter', function(req,res){
+   counter=counter+1;
+   res.send(counter.toString());
 });
 
 
