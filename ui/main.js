ef5fc04dab0b417bb75c2339ed5af991472a749f
mashroor1in <mashroor1in@gmail.com>
2016-10-08 13:56:58 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -37,8 +37,8 @@ submit.onclick=function(){
     var list='';
     for(var i=0; i<names.length; i++)
     {
-        list += '<li>' + names[i] + '</li>';
-        
+        list = '<li>' + names[i] + '</li>';
+        list=list+1;
     }
     var ul= document.getElementById('namelist');
     ul.innerHTML=list;
