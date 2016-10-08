c031267252bf1a026fb72a7a6084a4036b6eeedd
mashroor1in <mashroor1in@gmail.com>
2016-10-08 13:47:04 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,6 +30,15 @@ var name=nameInput.value;
 var submit=document.getElementById('submit_bttn');
 
 submit.onclick=function(){
+    //Make a request to the server and send  the name
     
-    
+    //capture a list of name and renders it as a list
+    var name=['name1','name2','name3','name4'];
+    var list='';
+    for(var i=0; i<names.length; i++)
+    {
+        list += '<li>' + names[i] + '</li>';
+    }
+    var ul= document.getElementById('namelist');
+    ul.innerHTML=list;
 };
