edae771aa82d751dc16f53ee0349a0281f5b4743
mashroor1in <mashroor1in@gmail.com>
2016-10-09 09:55:24 +0000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -122,7 +122,7 @@ var names= [];
 app.get('/submit-name/:name', function(req, res){
     var name=req.params.name;
     
-    name.push(name);
+    names.push(name);
     res.send(JSON.stringify(names));
     
     
