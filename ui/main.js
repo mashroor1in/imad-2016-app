d858099aba89411e3c2b4e43d6441cae16f07bd0
mashroor1in <mashroor1in@gmail.com>
2016-10-10 07:58:17 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,8 +25,7 @@ button.onclick = function(){
 };
 
 
-var nameInput=document.getElementById('name');
-var name=nameInput.value;
+
 var submit=document.getElementById('submit_bttn');
 
 submit.onclick=function(){
@@ -56,6 +55,8 @@ submit.onclick=function(){
    
    
    };
+   var nameInput=document.getElementById('name');
+   var name=nameInput.value;
    request.open('GET', 'http://mashroor1in.imad.hasura-app.io/submit-name?name=' + name, true);
    request.send(null);
     //Make a request to the server and send  the name
