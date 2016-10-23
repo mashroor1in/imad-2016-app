cf2afb2145198baf5faf3c2ab97f20d4777b07d1
mashroor1in <mashroor1in@gmail.com>
2016-10-23 18:33:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config={
+    user: 'mashroor1in',
+    database: 'mashroor1in',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password: process.env.DB_PASSWORD
+    
+}
 
 var app = express();
 app.use(morgan('combined'));
@@ -90,6 +100,20 @@ function createTemplate (data){
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+var pool=new Pool(config);
+
+app.get('/test-db', function(req,res){
+    //make a select statement
+    pool.query('Select * from test', function(err, result){
+        if(err){
+            res.status(500).send(err.toString());
+        }else{
+            res.send(JSON.stringify(result));
+        }
+        
+    });
+   
 });
 
 var counter=0;
